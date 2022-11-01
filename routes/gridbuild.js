ed34459bbeef348d7ca0522daae2f3547d896100
Srinidhi Balmuri <balmurisrinidhi@gmail.com>
2022-11-01 18:26:29 -0500

    gridbuild

--- routes/gridbuild.js
+++ routes/gridbuild.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next) {
     let query = req.query
 console.log(`rows ${query.rows}`)
 console.log(`cols ${query.cols}`)
+var rows=Number(query.rows)
+var cols=Number(query.cols)
   res.render('gridbuild', { title: 'Grid Build',query: query });
 });
 
